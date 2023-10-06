8cf464986ef5eaa8e6fd0e456dbf30f6f2206c56
mujibat <oshikoyadolapo13@gmail.com>
2023-10-06 03:06:44 +0100

    chore: forge init

--- contracts/MerkleTree.sol
+++ contracts/MerkleTree.sol
@@ -1,16 +1,17 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity 0.8.19;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
-import "./IToken.sol";
 
-contract Merkle{
-    IWordSanctuary nftAddr;
+
+contract MerkleTree is ERC20{
+    
     bytes32 public merkleRoot = 0x36aa3b65c321d69df189e026faa86d44b415330c17ac0e26045ead2284c63a6b;
     mapping(address => bool) public whitelistClaimed;
 
-    constructor(IWordSanctuary _nftAddr) {
-        nftAddr = _nftAddr;
+    constructor(bytes32 _merkleRoot) ERC20("MERKLE", "MRKL"){
+        merkleRoot = _merkleRoot;
     }
         
     function checkInWhitelist(bytes32[] calldata proof) public {
@@ -20,7 +21,8 @@ contract Merkle{
             MerkleProof.verify(proof, merkleRoot, leaf),
             "You are not among the whitelisted Addresses"
         );
-        nftAddr.mint(msg.sender, 1000);
         whitelistClaimed[msg.sender] = true;
+        _mint(msg.sender, 1000);
+        
     }
 }
